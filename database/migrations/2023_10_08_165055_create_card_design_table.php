33d240f1fe2d2705272c9b54d306401297103b6f
Harry <mr.harpreetsidhu@gmail.com>
2023-10-09 08:08:01 +0100

    upcode

---
+++ database/migrations/2023_10_08_165055_create_card_design_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('card_designs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable()->default(0);
+            $table->text('image_path')->nullable();
+            $table->enum('level', ['front', 'back'])->nullable()->default('front');
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->integer('deleted_by')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('card_designs');
+    }
+};

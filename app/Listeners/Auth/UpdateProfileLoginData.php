9f34ef9f638b2823f67806f8e2a9d06ca6bca552
rzhowell <rzhowell@owlishanalytics.com>
2023-12-13 11:49:06 +0100

    Login process now checks for custom pricing

--- app/Listeners/Auth/UpdateProfileLoginData.php
+++ app/Listeners/Auth/UpdateProfileLoginData.php
@@ -3,6 +3,7 @@
 namespace App\Listeners\Auth;
 
 use App\Events\Auth\UserLoginSuccess;
+use App\Models\Setting;
 use Carbon\Carbon;
 
 class UpdateProfileLoginData
@@ -37,6 +38,31 @@ class UpdateProfileLoginData
                 $user_profile->last_ip = $request->getClientIp();
                 $user_profile->login_count = $user_profile->login_count + 1;
                 $user_profile->save();
+                if(isset($user->card_written_pricing_less_than_equal_to_100)){
+                    Setting::set("card_written_pricing_less_then_equal_to_100", $user->card_written_pricing_less_than_equal_to_100);
+                } else {
+                    Setting::set("card_written_pricing_less_then_equal_to_100", 3.49);
+                }
+                if(isset($user->card_written_pricing_101_to_500)){
+                    Setting::set("card_written_pricing_101_to_500", $user->card_written_pricing_101_to_500);
+                } else {
+                    Setting::set("card_written_pricing_101_to_500", 2.99);
+                }
+                if(isset($user->card_written_pricing_501_to_1000)){
+                    Setting::set("card_written_pricing_501_to_1000", $user->card_written_pricing_501_to_1000);
+                } else {
+                    Setting::set("card_written_pricing_501_to_1000", 2.49);
+                }
+                if(isset($user->card_written_pricing_1001_to_2000)){
+                    Setting::set("card_written_pricing_1001_to_2000", $user->card_written_pricing_1001_to_2000);
+                } else {
+                    Setting::set("card_written_pricing_1001_to_2000", 2.29);
+                }
+                if(isset($user->card_written_pricing_greater_2000)){
+                    Setting::set("card_written_pricing_greater_2000", $user->card_written_pricing_greater_2000);
+                } else {
+                    Setting::set("card_written_pricing_greater_2000", 1.99);
+                }
             }
         } catch (\Exception $e) {
             logger()->error($e);
